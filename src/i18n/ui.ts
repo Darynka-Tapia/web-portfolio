244900d9d1416206f000ca2917ceeb0b7b3dca4a
Darynka Tapia <darytapia09@gmail.com>
2023-11-20 22:20:52 -0600

    add i8n

---
+++ src/i18n/ui.ts
@@ -0,0 +1,50 @@
+export const languages = {
+  en: 'English',
+  fr: 'Français',
+  es: 'Spanish',
+};
+
+export const defaultLang = 'en';
+
+export const ui = {
+  en: {
+    'about.greats' : "Hi there, I'm ",
+    'about.area' : "Web developer",
+    'about.experience' : "with more than 4 years of experience.",
+    'about.self-taught-first' : "I'm a",
+    'about.self-taught' : "self-taught",
+    'about.self-taught-end' : "person who likes to be constantly learning, with a current focus on the Frontend area.",
+    'about.call-action' : "Let's talk",
+    'about.download' : "Download CV",
+    'currently.paragraphe-1' : "I'm currently learning more about front-end and system design",
+    'currently.paragraphe-2' : "I'm currently working with frontend (TypeScript and Vue.js)",
+    'currently.paragraphe-3' : "Ask me about frontend and web design",
+    'skills.title' : "My Skills",
+  },
+  fr: {
+    'about.greats' : "Salut, Je m'appelle ",
+    'about.experience' : "avec plus de 4 ans d'expérience.",
+    'about.self-taught-first' : "Je suis une personne",
+    'about.self-taught' : "autodidacte",
+    'about.self-taught-end' : "qui aime apprendre constamment, avec une spécialisation actuelle dans le domaine Frontend.",
+    'about.call-action' : "On va parler",
+    'about.download' : "Télécharger le CV",
+    'currently.paragraphe-1' : "J'en apprends actuellement davantage sur le front-end et system design",
+    'currently.paragraphe-2' : "Je travaille actuellement avec le frontend (TypeScript et Vue.js)",
+    'currently.paragraphe-3' : "Posez-moi des questions sur le frontend et web design",
+    'skills.title' : "Mes compétences",
+  },
+  es: {
+    'about.greats' : "Hola, Me llamo ",
+    'about.experience' : "con más de 4 años de expereincia.",
+    'about.self-taught-first' : "Soy una persona",
+    'about.self-taught' : "autodidacta",
+    'about.self-taught-end' : "a la que le gusta estar en constante aprendizaje, con enfoque actual en el área Frontend.",
+    'about.call-action' : "Hablemos",
+    'about.download' : "Descargar CV",
+    'currently.paragraphe-1' : "Actualmente estoy aprendiendo sobre front-end y system design",
+    'currently.paragraphe-2' : "Actualmente trabajo en frontend (TypeScript and Vue.js)",
+    'currently.paragraphe-3' : "Preguntame sobre frontend y web design",
+    'skills.title' : "Mis Habilidades",
+  },
+} as const;
